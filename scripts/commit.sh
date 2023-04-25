afd445e7a058957bd7fbd4f42bd1cf033c8cad82
Nickolsky <romb.intu@gmail.com>
2023-04-25 18:29:56 +0300

    0.1.3.2

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,7 +3,7 @@
 set -eu
 
 python3 -m poetry version patch
-VERSION=$(cat pyproject.toml | head | grep version | cut -d '=' -f 2)
+VERSION=$(cat pyproject.toml | head | grep version | awk '{print $3}' | cut -d '"' -f 2)
 cp ./.gitignore ./.dockerignore
 git add *
 git commit -m $VERSION
